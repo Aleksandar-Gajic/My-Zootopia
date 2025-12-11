36cdd65f6d2fc66d9d1b9a3a06c823b98370d1e9
Aleksandar-Gajic <aleksandargajic@ymail.com>
2025-12-11 13:24:03 +0100

    generate card item for every animal

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,33 +1,29 @@
 import json
 
 def load_data(file_path):
-  """ Loads a JSON file """
-  with open(file_path, "r") as handle:
-    return json.load(handle)
+    with open(file_path, "r") as handle:
+        return json.load(handle)
 
 animals_data = load_data('animals_data.json')
 
 output = ''
 
 for animal in animals_data:
+    output += '<li class="cards__item">\n'
 
-    # Name
     if "name" in animal:
-        output += f"Name: {animal['name']}\n"
+        output += f"Name: {animal['name']}<br/>\n"
 
-    # Diet
     if "characteristics" in animal and "diet" in animal["characteristics"]:
-        output += f"Diet: {animal['characteristics']['diet']}\n"
+        output += f"Diet: {animal['characteristics']['diet']}<br/>\n"
 
-    # First location
     if "locations" in animal and len(animal["locations"]) > 0:
-        output += f"Location: {animal['locations'][0]}\n"
+        output += f"Location: {animal['locations'][0]}<br/>\n"
 
-    # Type
     if "characteristics" in animal and "type" in animal["characteristics"]:
-        output += f"Type: {animal['characteristics']['type']}\n"
+        output += f"Type: {animal['characteristics']['type']}<br/>\n"
 
-    output += "\n"
+    output += '</li>\n\n'
 
 with open('animals_template.html', 'r') as f:
     html_template = f.read()
