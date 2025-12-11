8409c3d41d88e02cc1cd98211b6c211e5f41e898
Aleksandar-Gajic <aleksandargajic@ymail.com>
2025-12-11 13:36:17 +0100

    code makeup

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -4,12 +4,8 @@ def load_data(file_path):
     with open(file_path, "r") as handle:
         return json.load(handle)
 
-animals_data = load_data('animals_data.json')
-
-output = ''
-
-for animal in animals_data:
-    output += '<li class="cards__item">\n'
+def serialize_animal(animal):
+    output = '<li class="cards__item">\n'
 
     if "name" in animal:
         output += f'  <div class="card__title">{animal["name"]}</div>\n'
@@ -27,6 +23,13 @@ for animal in animals_data:
 
     output += '  </p>\n'
     output += '</li>\n\n'
+    return output
+
+animals_data = load_data('animals_data.json')
+
+output = ''
+for animal in animals_data:
+    output += serialize_animal(animal)
 
 with open('animals_template.html', 'r') as f:
     html_template = f.read()
